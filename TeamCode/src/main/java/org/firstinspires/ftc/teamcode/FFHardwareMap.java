c4393d9d9069a01da5d67654d8de8a4f2a82d04e
lilpickens <hummere03@gmail.com>
2021-12-07 17:11:37 -0500

    Hardware Map Additions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFHardwareMap.java
@@ -18,15 +18,25 @@ public class FFHardwareMap
     public DcMotor br;
     public DcMotor bl;
 
+    public DcMotor arm;
+    public DcMotor lift;
+
     //DEFINE SERVOS
-    public Servo lookieLookie;
+    public Servo cap;
+    public Servo capArm;
+    public Servo deposit;
+    public Servo gate;
+    public CRServo collect;
+
+
+    //public Servo lookieLookie;
 
 
 
     //CONSTANTS
 
-    public final double[] lookieLimits = {0.17, 1};
-    public final double lookieCentered = 0.5;
+    //public final double[] lookieLimits = {0.17, 1};
+    //public final double lookieCentered = 0.5;
 
     /* local OpMode members. */
     HardwareMap hwMap =  null;
@@ -49,35 +59,49 @@ public class FFHardwareMap
         br = hwMap.get(DcMotor.class, "br");
         bl = hwMap.get(DcMotor.class, "bl");
 
-        lookieLookie = hwMap.get(Servo.class, "lookieLookie");
+        arm = hwMap.get(DcMotor.class, "collectArm");
+        lift = hwMap.get(DcMotor.class, "lift");
 
-        //aim = ahwMap.servo.get("aim");
-        //fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        cap = hwMap.get(Servo.class, "cap");
+        capArm = hwMap.get(Servo.class, "capArm");
+        deposit = hwMap.get(Servo.class, "deposit");
+        gate = hwMap.get(Servo.class, "gate");
+        collect = hwMap.get(CRServo.class, "collect");
+
+        //lookieLookie = hwMap.get(Servo.class, "lookieLookie");
+
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         fr.setDirection(DcMotorSimple.Direction.FORWARD);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.FORWARD);
 
-
-        //flyWheel.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         //robot.init(hardwareMap);
 
-        lookieLookie.setPosition(lookieCentered);
+        //lookieLookie.setPosition(lookieCentered);
 
     }
 }
